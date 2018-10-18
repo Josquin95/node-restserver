04b08b81782cdfab5798f4fb0494b6f26adb269d
Jose Luis Osorio Quintero <jose.osorio1@correo.icesi.edu.co>
2018-10-17 19:24:51 -0500

    se agregan ultimas modificaciones

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,36 +11,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('Get usuario')
-});
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            OK: false,
-            Descripcion: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('Delete usuario')
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
